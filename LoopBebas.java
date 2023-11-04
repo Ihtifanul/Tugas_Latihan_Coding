c7138d5c8d01824b60fa44850d1d507d14449717
Ihtifanul <149654048+Ihtifanul@users.noreply.github.com>
2023-11-04 09:47:28 +0700

    LoopBebas.java

--- LoopBebas.java
+++ LoopBebas.java
@@ -1,1 +1,19 @@
+public class LoopBebas {
+    public static void main(String[] args) {
+        int i = 10;
 
+        while (i > 0) {
+            System.out.println("Waktu yang tersisa tinggal " + i + " Detik");
+            i--;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+            if (i == 0) {
+        System.out.println("Waktu telah habis!");
+        }
+    }
+}
