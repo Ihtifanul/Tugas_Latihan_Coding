0c296ddb9f1e8c3d3c961fe250567edefe29152e
Ihtifanul <149654048+Ihtifanul@users.noreply.github.com>
2023-11-04 10:20:17 +0700

    Add files via upload

---
+++ HitungMundur.java
@@ -0,0 +1,21 @@
+public class HitungMundur {
+    public static void main(String[] args) {
+        int i = 10;
+
+        while (i > 0) {
+            System.out.println("Anda hanya punya waktu " + i + " Detik");
+            i--;
+
+            try {
+                Thread.sleep(1000);
+                if (i == 5){
+                    System.out.println("Waktu tinggal "+ i + " Detik Lagi!" );
+                } else if (i == 0) {
+                    System.out.println("Waktu telah habis");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
